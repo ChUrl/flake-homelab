d63a920f242202539a44945dbede94d9b3b512dc
Christoph Urlacher <christoph.urlacher@protonmail.com>
2023-09-01 11:38:46 +0200

    Add radarr service

---
+++ ServeNix/services/radarr.nix
@@ -0,0 +1,33 @@
+{config, lib, pkgs, ...}:
+
+{
+  virtualisation.oci-containers.containers.radarr = {
+    image = "linuxserver/radarr:latest";
+    autoStart = true;
+
+    dependsOn = [
+      "sabnzbd"
+      "prowlarr"
+    ];
+
+    ports = [
+      "7878:7878"
+    ];
+
+    volumes = [
+      "/media/Usenet:/downloads"
+      "/media/Movie:/movies"
+      "radarr_config:/config"
+    ];
+
+    environment = {
+      PUID = "3001";
+      PGID = "3001";
+      TZ = "Europe/Berlin";
+    };
+
+    extraOptions = [
+      # "--restart=always" # Conflicts with NixOS' default of using --rm
+    ];
+  };
+}
