38a1771b7eee96e15528ab0b78c89674d4f45b91
Christoph Urlacher <christoph.urlacher@protonmail.com>
2023-09-01 11:38:50 +0200

    Add prowlarr service

---
+++ ServeNix/services/prowlarr.nix
@@ -0,0 +1,28 @@
+{config, lib, pkgs, ...}:
+
+{
+  virtualisation.oci-containers.containers.prowlarr = {
+    image = "linuxserver/prowlarr:latest";
+    autoStart = true;
+
+    dependsOn = [];
+
+    ports = [
+      "9696:9696"
+    ];
+
+    volumes = [
+      "prowlarr_config:/config"
+    ];
+
+    environment = {
+      PUID = "1000";
+      PGID = "1000";
+      TZ = "Europe/Berlin";
+    };
+
+    extraOptions = [
+      # "--restart=always" # Conflicts with NixOS' default of using --rm
+    ];
+  };
+}
