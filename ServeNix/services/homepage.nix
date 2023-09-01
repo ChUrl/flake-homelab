484c7b42545ba5ee141bbc0b08d7def34cbc8574
Christoph Urlacher <christoph.urlacher@protonmail.com>
2023-09-01 11:38:55 +0200

    Add homepage service

---
+++ ServeNix/services/homepage.nix
@@ -0,0 +1,25 @@
+{config, lib, pkgs, ...}:
+
+{
+  virtualisation.oci-containers.containers.homepage = {
+    image = "ghcr.io/benphelps/homepage:latest";
+    autoStart = true;
+
+    dependsOn = [];
+
+    ports = [
+      "3000:3000"
+    ];
+
+    volumes = [
+      "homepage_config:/app/config"
+      "/var/run/docker.sock:/var/run/docker.sock:ro"
+    ];
+
+    environment = {};
+
+    extraOptions = [
+      # "--restart=always" # Conflicts with NixOS' default of using --rm
+    ];
+  };
+}
