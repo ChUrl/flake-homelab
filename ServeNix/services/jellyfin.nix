c8d958a0c6a3c02e4d71fbc69a029daf00f471be
Christoph Urlacher <christoph.urlacher@protonmail.com>
2023-09-01 11:38:59 +0200

    Add jellyfin service

---
+++ ServeNix/services/jellyfin.nix
@@ -0,0 +1,31 @@
+{config, lib, pkgs, ...}:
+
+{
+  virtualisation.oci-containers.containers.jellyfin = {
+    image = "linuxserver/jellyfin:latest";
+    autoStart = true;
+
+    dependsOn = [];
+
+    ports = [
+      "8096:8096"
+    ];
+
+    volumes = [
+      "/media/Music:/data/music"
+      "/media/Show:/data/tvshows"
+      "/media/Movie:/data/movies"
+      "jellyfin_config:/config"
+    ];
+
+    environment = {
+      PUID = "3000";
+      PGID = "3000";
+      TZ = "Europe/Berlin";
+    };
+
+    extraOptions = [
+      # "--restart=always" # Conflicts with NixOS' default of using --rm
+    ];
+  };
+}
