ad0b709537706b8c2557b69f6dda0d5a51ac2f03
Christoph Urlacher <christoph.urlacher@protonmail.com>
2023-09-10 11:24:59 +0200

    Add nextcloud service

---
+++ ServeNix/services/nextcloud.nix
@@ -0,0 +1,32 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  virtualisation.oci-containers.containers.netflix = {
+    image = "linuxserver/nextcloud:latest";
+    autoStart = true;
+
+    dependsOn = [
+      "pihole"
+    ];
+
+    ports = [
+      "443:443"
+    ];
+
+    volumes = [
+      "nextcloud_config:/config"
+      "nextcloud_data:/data"
+    ];
+
+    environment = {
+      PUID = "1000";
+      PGID = "1000";
+      TZ = "Europe/Berlin";
+    };
+
+    extraOptions = [];
+  };
+}
