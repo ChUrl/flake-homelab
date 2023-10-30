84f3ec569b73af23c6f80d4af9c22d224948aae5
Christoph Urlacher <christoph.urlacher@protonmail.com>
2023-10-30 11:41:08 +0100

    Enable jellyfin hardware transcoding

--- ServeNix/services/jellyfin.nix
+++ ServeNix/services/jellyfin.nix
@@ -28,11 +28,13 @@
       PUID = "3000";
       PGID = "3000";
       TZ = "Europe/Berlin";
+
       NVIDIA_VISIBLE_DEVICES = "all";
       NVIDIA_DRIVER_CAPABILITIES = "all";
     };
 
     extraOptions = [
+      "--privileged"
       "--gpus=all"
       "--net=behind-nginx"
     ];
