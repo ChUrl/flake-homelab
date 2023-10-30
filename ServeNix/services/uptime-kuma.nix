83cffff875ef84f5359118a435ea8610b012dcad
Christoph Urlacher <christoph.urlacher@protonmail.com>
2023-10-30 11:41:45 +0100

    Add uptime-kuma service

---
+++ ServeNix/services/uptime-kuma.nix
@@ -0,0 +1,36 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  virtualisation.oci-containers.containers.uptime-kuma = {
+    image = "louislam/uptime-kuma:latest";
+    autoStart = true;
+
+    dependsOn = [
+      # "pihole"
+    ];
+
+    ports = [];
+
+    volumes = [
+      "uptime-kuma_config:/app/data"
+
+      "/var/run/docker.sock:/var/run/docker.sock"
+    ];
+
+    environment = {
+      # PUID = "1000";
+      # PGID = "1000";
+      # TZ = "Europe/Berlin";
+      # NVIDIA_VISIBLE_DEVICES = "all";
+      # NVIDIA_DRIVER_CAPABILITIES = "all";
+    };
+
+    extraOptions = [
+      # "--gpus=all"
+      "--network=behind-nginx"
+    ];
+  };
+}
