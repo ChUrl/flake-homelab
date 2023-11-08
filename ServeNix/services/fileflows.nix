4e28557396fc97d258f850a64d661e7ee54dbffc
Christoph Urlacher <christoph.urlacher@protonmail.com>
2023-11-08 23:40:21 +0100

    Second fileflows service

---
+++ ServeNix/services/fileflows.nix
@@ -0,0 +1,43 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  virtualisation.oci-containers.containers.fileflows = {
+    image = "revenz/fileflows:latest";
+    autoStart = true;
+
+    dependsOn = [
+      # "pihole"
+    ];
+
+    ports = [
+      # "5000:5000"
+    ];
+
+    volumes = [
+      "/media/Movie:/media/Movie"
+      "/media/Show:/media/Show"
+
+      "fileflows_temp:/temp"
+      "fileflows_logs:/app/Logs"
+      "fileflows_config:/app/Data"
+
+      "/var/run/docker.sock:/var/run/docker.sock:ro"
+    ];
+
+    environment = {
+      TZ = "Europe/Berlin";
+
+      NVIDIA_VISIBLE_DEVICES = "all";
+      NVIDIA_DRIVER_CAPABILITIES = "all";
+    };
+
+    extraOptions = [
+      "--privileged" # Helps with CUDA issues
+      "--gpus=all"
+      "--net=behind-nginx"
+    ];
+  };
+}
