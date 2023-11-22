b42289045a1948dbd5f172326478562fcbd0561c
Christoph Urlacher <christoph.urlacher@protonmail.com>
2023-11-22 10:25:24 +0100

    Remove dind stuff from gitea-runner.nix

--- ServeNix/services/gitea-runner.nix
+++ ServeNix/services/gitea-runner.nix
@@ -5,29 +5,16 @@
   ...
 }: {
   virtualisation.oci-containers.containers.gitea-runner = {
-    # Question: For gitea/act_runner dind set config.yaml/docker_host to "unix:///var/run/user/1000/docker.sock"?
-    image = "gitea/act_runner:latest";
-    # image = "vegardit/gitea-act-runner:dind-latest";
-
+    image = "gitea/act_runner:latest"; # NOTE: vegardit has other runner images
     autoStart = true;
 
-    dependsOn = [
-      # "pihole"
-    ];
+    dependsOn = [];
 
     ports = [];
 
     volumes = [
       "gitea-runner_data:/data"
       "gitea-runner_config:/config" # Managed by env variables for vegardit image
-
-      # For rootless-dind
-      # "gitea-runner_rootless-docker-auth:/home/rootless/.docker"
-
-      # Cache dind pulled images
-      # "gitea-runner_overlay2:/var/lib/docker/overlay2"
-      # "gitea-runner_image:/var/lib/docker/image"
-
     
       "/var/run/docker.sock:/var/run/docker.sock" # Disable for dind
     ];
@@ -42,12 +29,6 @@
 
       # NOTE: This token is invalid, when re-registering is needed it has to be refreshed
       GITEA_RUNNER_REGISTRATION_TOKEN = "Mq6wr0dPthqDij3iaryP8s5VYZA5kPfOQbHA6wm6";
-
-      # These are for the specific vegardit/gitea-act-runner image
-      # GITEA_RUNNER_LOG_LEVEL = "debug";
-      # GITEA_INSTANCE_INSECURE = "true";
-      # GITEA_RUNNER_JOB_CONTAINER_NETWORK = "host"; # "host" for dind, "behind-nginx" otherwise
-      # GITEA_RUNNER_JOB_CONTAINER_PRIVILEGED = "true"; # Enable for dind
     };
 
     extraOptions = [
